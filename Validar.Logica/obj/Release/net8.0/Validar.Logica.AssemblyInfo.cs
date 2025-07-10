d059112825b24743497d9045d95f1eaefc747430
AdrianZa1 <e0942291741@live.uleam.edu.ec>
2025-07-10 17:41:27 -0500

    Build limpio y sincronizado

--- Validar.Logica/obj/Release/net8.0/Validar.Logica.AssemblyInfo.cs
+++ Validar.Logica/obj/Release/net8.0/Validar.Logica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Validar.Logica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4227512cb6d4ff6a39faeadfebda13b9613837c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d8cc4de06f7f5289f4877f5f5dfb6a0f8d7cb1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Validar.Logica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Validar.Logica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
